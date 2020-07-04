AkselSoft-Hub/moodtracker

commit 1701dc320407061d91af664e1e9dea4d4acf29b2

fix(*): only load notes from storage after user is loaded

client/src/components/hooks/useStorage.ts
@@ -6,19 +6,18 @@ export default function useStorage() {
   const dispatch = React.useContext(DispatchContext);
   const state = React.useContext(StateContext);
 
-  React.useEffect(
-    () =>
-      void (async () => {
-        try {
-          const events = await storage.getEvents();
-          if (events)
-            dispatch({ type: "events/loadFromStorage", payload: events });
-        } finally {
-          dispatch({ type: "storage/loaded" });
-        }
-      })(),
-    []
-  );
+  React.useEffect(() => {
+    if (!state.userEmail) return;
+    void (async () => {
+      try {
+        const events = await storage.getEvents();
+        if (events)
+          dispatch({ type: "events/loadFromStorage", payload: events });
+      } finally {
+        dispatch({ type: "storage/loaded" });
+      }
+    })();
+  }, [state.userEmail]);
 
   React.useEffect(() => {
     if (state.isStorageLoading) return;

commit 2fcf1c03556ff4580c074c61428324b044cc8ee6

fix(client): fix infinite spinner bug caused by 1701dc320407061d91af664e1e9dea4d4acf29b2

client/src/components/hooks/useStorage.ts
@@ -7,7 +7,8 @@ export default function useStorage() {
   const state = React.useContext(StateContext);
 
   React.useEffect(() => {
-    if (!state.user.email) return;
+    if (state.user.loading || !state.isStorageLoading) return;
+    if (!state.user.email) return dispatch({ type: "storage/loaded" });
     void (async () => {
       try {
         const events = await storage.getEvents();
@@ -17,7 +18,7 @@ export default function useStorage() {
         dispatch({ type: "storage/loaded" });
       }
     })();
-  }, [state.user.email]);
+  }, [state.isStorageLoading, state.user.email, state.user.loading]);
 
   React.useEffect(() => {
     if (state.isStorageLoading) return;